444e448ac9753ce28ee6e377d749eb96d0b9c083
Simon Whitaker <swhitaker@fb.com>
2017-09-05 06:18:07 +0100

    Upgrade to Swift 4

--- Bag/Bag.swift
+++ Bag/Bag.swift
@@ -8,36 +8,30 @@
 
 import Foundation
 
-struct Bag<T: Hashable> {
-  fileprivate var storage: Dictionary<T, Int>
-  private var _count: Int = 0
+public struct Bag<T: Hashable> {
+  private var storage: [T: UInt] = [:]
+  public private(set) var count: UInt = 0
 
-  init() {
-    storage = Dictionary<T, Int>()
+  public init() {}
+
+  public mutating func add (_ elem: T) {
+    storage[elem, default: 0] += 1
+    count += 1
   }
 
-  mutating func add (_ elem: T) {
-    if let currentCount = self.storage[elem] {
-      self.storage[elem] = currentCount + 1;
-    } else {
-      self.storage[elem] = 1
-    }
-    _count += 1
-  }
-
-  mutating func remove (_ elem: T) {
-    if let currentCount = self.storage[elem] {
+  public mutating func remove (_ elem: T) {
+    if let currentCount = storage[elem] {
       if currentCount > 1 {
-        self.storage[elem] = currentCount - 1
+        storage[elem] = currentCount - 1
       } else {
-        self.storage.removeValue(forKey: elem)
+        storage.removeValue(forKey: elem)
       }
-      _count -= 1
+      count -= 1
     }
   }
 
-  func isSubBag (of superbag: Bag<T>) -> Bool {
-    for (key, count) in self.storage {
+  public func isSubBag (of superbag: Bag<T>) -> Bool {
+    for (key, count) in storage {
       let superbagcount = superbag.storage[key] ?? 0
       if count > superbagcount {
         return false
@@ -46,19 +40,24 @@ struct Bag<T: Hashable> {
     return true
   }
 
-  var count: Int {
-    get {
-      return _count
-    }
+  public func count(for key: T) -> UInt {
+    return storage[key] ?? 0
   }
 
-  func count(for key: T) -> Int {
-    return self.storage[key] ?? 0
+  public var allItems: [T] {
+    var result = [T]()
+    for (key, count) in storage {
+      for _ in 0 ..< count {
+        result.append(key)
+      }
+    }
+    return result
   }
 }
 
+// MARK: - Equatable
 extension Bag: Equatable {
-  static func == (lhs: Bag<T>, rhs: Bag<T>) -> Bool {
+  public static func == (lhs: Bag<T>, rhs: Bag<T>) -> Bool {
     if lhs.storage.count != rhs.storage.count {
       return false
     }
